b2aabc83de3968b81b15c79c8fb0d5f94785dc01
Jack Thomasson <4302889+jkt628@users.noreply.github.com>
2024-09-20 07:11:26 -0400

    simple reformat

--- custom-phase.bash
+++ custom-phase.bash
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
 function loadparams() {
-    local mpt=''
-    [ "$SDMNSPAWN" == "Phase0" ] && mpt=$SDMPT
-    # shellcheck disable=SC1091
-    source "$mpt/etc/sdm/sdm-readparams"
+	local mpt=''
+	[ "$SDMNSPAWN" == "Phase0" ] && mpt=$SDMPT
+	# shellcheck disable=SC1091
+	source "$mpt/etc/sdm/sdm-readparams"
 }
 
 pfx=$(basename "$0")
 loadparams
 
 phase0() {
-  logtoboth "* $pfx install overlay"
-  mapfile -d '' files < <(cd rootfs && find . -print0 \( -type f -o -type l \))
+	logtoboth "* $pfx install overlay"
+	mapfile -d '' files < <(cd rootfs && find . -print0 \( -type f -o -type l \))
 	for file in "${files[@]}"; do
-    local args=()
-    case "$file" in
-      */home/jkt/*) args=(--owner=1000 --group=1000);;&
-      */etc/ssh/*) args=(--mode=600);;&
-      */.gitignore) install -d "${args[@]}" "${file%/.gitignore}";;
-      *) install "${args[@]}" -D "rootfs/$file" "$SDMPT/$file";;
-    esac
-  done
+		local args=()
+		case "$file" in
+		*/home/jkt/*) args=(--owner=1000 --group=1000) ;;&
+		*/etc/ssh/*) args=(--mode=600) ;;&
+		*/.gitignore) install -d "${args[@]}" "${file%/.gitignore}" ;;
+		*) install "${args[@]}" -D "rootfs/$file" "$SDMPT/$file" ;;
+		esac
+	done
 }
 
 phase1() {
-  logtoboth "* $pfx disable dtoverlay"
-  sed -i -e '/vc4-kms-v3d/s/^/#/' /boot/config.txt
+	logtoboth "* $pfx disable dtoverlay"
+	sed -i -e '/vc4-kms-v3d/s/^/#/' /boot/config.txt
 	apt -y remove --purge nano
 }
 
@@ -52,7 +52,7 @@ EOF
 }
 
 case "$1" in
-  0) phase0;;
-  1) phase1;;
-  *) postinstall;;
+0) phase0 ;;
+1) phase1 ;;
+*) postinstall ;;
 esac
